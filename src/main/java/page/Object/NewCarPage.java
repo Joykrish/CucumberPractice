2b3c29773472e557addecbf354e6875816fa80ca
Joykrish <jaykishorsp@gmail.com>
2024-09-12 14:40:48 +0530

    message

--- src/main/java/page/Object/NewCarPage.java
+++ src/main/java/page/Object/NewCarPage.java
@@ -1,7 +1,12 @@
 package page.Object;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import Utils.SeleniumDriver;
 import page.Base.BasePage;
 
 public class NewCarPage extends BasePage {
@@ -13,8 +18,9 @@ public class NewCarPage extends BasePage {
 	public BMWPage gotoBMW() throws InterruptedException {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0,150)", "");
-		Thread.sleep(10000);
-		click("BMW_Xpath");
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		WebElement we=wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(SeleniumDriver.or.getProperty("BMW_Xpath"))));
+		we.click();
 		return new BMWPage();
 	}
 
