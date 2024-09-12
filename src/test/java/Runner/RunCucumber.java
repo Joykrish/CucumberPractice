d129a9bbc4bdfdcdc1252dafdc750800aa6f0cd7
Joykrish <jaykishorsp@gmail.com>
2024-09-12 13:38:51 +0530

    changes

--- src/test/java/Runner/RunCucumber.java
+++ src/test/java/Runner/RunCucumber.java
@@ -3,7 +3,7 @@ package Runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/Features/AmericalExpress.feature", glue = { "StepDefinations" }, plugin = {
+@CucumberOptions(features = "src/test/java/Features/FindnewCar.feature", glue = { "StepDefinations" }, plugin = {
 		"html:target/cucumber-reports/cucumber-html-report.html",
 		 }, dryRun = false)
 public class RunCucumber extends AbstractTestNGCucumberTests {}
